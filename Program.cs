0c8f357a7c90ddebe6b3c6276adbbeb51eb53052
GGG <gggkiller2@gmail.com>
2024-04-10 18:21:18 -0300

    Add response compression to DI container.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddResponseCompression();
 
 builder.Services.AddHttpClient();
 builder.Services.AddDbContextFactory<AppDbContext>(options =>
